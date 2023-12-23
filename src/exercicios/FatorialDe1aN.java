f1505dd2d8b3e008fca00d9f045a5c590a446069
MiqueiasLima <miqueiaslima@outlook.com>
2023-12-22 22:34:24 -0300

    Adicionando fatorial de 0

--- src/exercicios/FatorialDe1aN.java
+++ src/exercicios/FatorialDe1aN.java
@@ -1,20 +1,24 @@
 package exercicios;
 
 /**
- *  Imprima	os	fatoriais	de	1	a	n.
+ * Imprima	os	fatoriais	de	1	a	n.
  */
 
 public class FatorialDe1aN {
     public static void main(String[] args) {
 
-        int entrada = 20;
+        int entrada = 10;
 
-        for(int i = 1; i <= entrada; i++){
-            long fatorial = 1;
-            for(int j = 1; j <= i; j++){
-                fatorial = fatorial * j;
+        if (entrada == 0) {
+            System.out.println("O fatorial de 0 é 1");
+        } else {
+            for (int i = 1; i <= entrada; i++) {
+                long fatorial = 1;
+                for (int j = 1; j <= i; j++) {
+                    fatorial = fatorial * j;
+                }
+                System.out.println("O fatorial de " + i + " é " + fatorial);
             }
-            System.out.println("O fatorial de "+i+ " é "+fatorial);
         }
     }
 }
