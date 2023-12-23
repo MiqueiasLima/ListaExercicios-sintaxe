f421fa70774c22f227d1b7e5bfa77852a635512d
MiqueiasLima <miqueiaslima@outlook.com>
2023-12-23 10:43:15 -0300

    Descrevendo atividade

--- src/exercicios/Fibonacci.java
+++ src/exercicios/Fibonacci.java
@@ -1,5 +1,12 @@
 package exercicios;
 
+/**
+ * 	 Imprima	 os	 primeiros	 números	 da	 série	 de	 Fibonacci	 até	 passar	 de	 100.	 A	 série	 de
+ * Fibonacci	é	a	seguinte:	0,	1,	1,	2,	3,	5,	8,	13,	21,	etc.	Para	calculá-la,	o	primeiro	elemento	vale	0,	o
+ * segundo	vale	1,	e	daí	por	diante.	O	n-ésimo	elemento	vale	o	(n-1)-ésimo	elemento	somado	ao	(n-2)-
+ * ésimo	elemento	(ex:	8	=	5	+	3).
+ */
+
 public class Fibonacci {
     public static void main(String... args) {
 
