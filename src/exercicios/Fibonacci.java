67df179ebde0f8c894ac23d1e3e37aa4d212bff3
MiqueiasLima <miqueiaslima@outlook.com>
2023-12-23 11:23:45 -0300

    Ajuste na atribuição de valor na variável "resultado"

--- src/exercicios/Fibonacci.java
+++ src/exercicios/Fibonacci.java
@@ -8,18 +8,18 @@ package exercicios;
  */
 
 public class Fibonacci {
-    public static void main(String... args) {
+    public static void main(String[] args) {
 
         int num1 = 0;
         int num2 = 0;
         int resultado;
         int entrada = 10;
 
-        if (entrada != 0) {
+        if (entrada > 0) {
             for (int i = 0; i < entrada; i++) {
                 if (i == 1) {
                     num2 = 1;
-                    resultado = num1 + num2;
+                    resultado = num2;
                 } else {
                     resultado = num1 + num2;
                     num1 = num2;
