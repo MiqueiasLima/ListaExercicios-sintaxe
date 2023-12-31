9817bdfbf51d80c3c0a1f711f0205de3d8c9539b
MiqueiasLima <miqueiaslima@outlook.com>
2023-12-31 19:48:22 -0300

    Triangulo de Floyd

--- src/exercicios/TrianguloDeFloyd.java
+++ src/exercicios/TrianguloDeFloyd.java
@@ -12,13 +12,11 @@ package exercicios;
 public class TrianguloDeFloyd {
     public static void main(String[] args) {
 
-        int num;
         int entrada = 5;
 
         for(int i = 1; i <= entrada; i++){
             for(int j = 1; j <= i; j++){
-                num = i * j;
-                System.out.print(num + " ");
+                System.out.print(i * j + " ");
             }
             System.out.println();
         }
