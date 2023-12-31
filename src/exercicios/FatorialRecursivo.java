2f7c1962fbad45cf5c1376795474fe4181f6caf6
MiqueiasLima <miqueiaslima@outlook.com>
2023-12-30 21:44:32 -0300

    Fatorial Recursivo de uma linha

--- src/exercicios/FatorialRecursivo.java
+++ src/exercicios/FatorialRecursivo.java
@@ -2,14 +2,9 @@ package exercicios;
 
 public class FatorialRecursivo {
 
-    private static long calculaFatorialRecursivo(int n){
-        if(n == 0 || n == 1){
-            return 1;
-        }else{
-            return n * calculaFatorialRecursivo(n - 1);
-        }
+    private static long calculaFatorialRecursivo(int n) {
+        return n == 0 || n == 1 ? 1 : n * calculaFatorialRecursivo(n - 1);
     }
-
 
     public static void main(String[] args) {
 
